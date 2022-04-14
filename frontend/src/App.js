45eab4cc4fac3aa5aa55575f5373be291e15dd4e
maint2802 <mai28022000@gmail.com>
2022-04-14 15:07:20 +0700

    fix login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
+
 import './App.scss';
 import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
-import Register from './components/Register/Register';
 import Navbar from './components/Navbar/Navbar';
 import Router from './routes/Router';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="App">
         <Navbar />
         <Router />
       </div>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 export default App;
