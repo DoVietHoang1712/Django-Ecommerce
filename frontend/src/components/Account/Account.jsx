f6226997b6b5301145bc2c3aaf9736657fbfce9d
maint2802 <mai28022000@gmail.com>
2022-04-15 17:30:15 +0700

    fix render infor user

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -3,25 +3,45 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Account.scss';
 const Account = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState({});
+  const [isLoading, SetLoading] = useState(true)
   useEffect(() => {
+    // var config = {
+    //   method: 'get',
+    //   url: 'http://localhost:8000/user/me',
+    //   headers: {
+    //     Authorization: 'Token ba263a79474576fff23ddc970418aefe38fd702c',
+    //   },
+    // };
+    // axios(config)
+    //   .then(function (response) {
+    //     console.log(response);
+    //     setData(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
     var config = {
       method: 'get',
       url: 'http://localhost:8000/user/me',
       headers: {
-        Authorization: 'Token ba263a79474576fff23ddc970418aefe38fd702c',
-      },
+        'Authorization': 'Token ba263a79474576fff23ddc970418aefe38fd702c'
+      }
     };
     axios(config)
       .then(function (response) {
-        console.log(response);
-        setData(response.data);
+        console.log(response.data.data);
+        setData(response.data.data)
+        SetLoading(false)
       })
       .catch(function (error) {
         console.log(error);
       });
   }, []);
-    console.log(data);
+  console.log(data);
+  let isEmpty = Object.keys(data).length === 0;
+  console.log(isEmpty)
+  const { id, username, date_joined, email, is_staff } = data
   return (
     <div className="account container">
       <div className="row">
@@ -37,8 +57,17 @@ const Account = () => {
             <Link to="/cart">My cart</Link>
           </div>
         </div>
+        <h3>My infor</h3>
         <div className="col-md-8 infor-user">
-          {/* <div>{data.data.username}</div> */}
+          {isLoading ? <div style={{ textAlign: 'left' }}>is loading data...</div> : (
+            <div style={{ textAlign: 'left' }}>
+              <div>Tên đăng nhâp: {username}</div>
+              <div>ID: {id}</div>
+              <div>Email: {email}</div>
+              <div>Ngày đăng ký: {date_joined}</div>
+              <div>Vị trí: {is_staff ? <sapn>Nhân viên</sapn> : <span>Khách hàng</span>}</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
