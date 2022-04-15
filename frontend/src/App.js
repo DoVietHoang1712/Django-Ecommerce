860e982364ef544c4f94993be8b2db38e95c9d17
maint2802 <mai28022000@gmail.com>
2022-04-15 16:21:15 +0700

    call api login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.scss';
 import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Router from './routes/Router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +12,19 @@ function App() {
         <Navbar />
         <Router />
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      {/* Same as */}
+      <ToastContainer />
     </BrowserRouter >
   );
 }
--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,61 +1,116 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './_register.scss';
+import { useState, useEffect } from 'react';
+import qs from 'qs';
+import axios from 'axios';
 const Register = () => {
+
+  const [register, setRegister] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    username: '',
+    password: ''
+  })
+  const [post, setPost] = useState({
+    username: '',
+    password: '',
+    email: ''
+  })
+
+  const { firstname, lastname, email, username, password } = register
+  const handleSubmit = () => {
+    // console.log(register)
+    setPost({ username, password, email })
+  }
+
+  useEffect(() => {
+    var data = qs.stringify({
+      'username': post.username,
+      'password': post.password,
+      'email': post.email
+    });
+    var config = {
+      method: 'post',
+      url: 'http://127.0.0.1:8000/auth/users/',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }, [post])
+  // console.log(post)
   return (
     <div className="container register">
       <div className="row cover mx-auto mt-4">
         <div className="col-md-12">
           <h1 className="mt-4 pt-4 text-center">Sign up</h1>
           <div className="row register_form-register pb-4">
-            <form action="" method="post" className=" mx-auto mt-3 col-10">
+            <div className=" mx-auto mt-3 col-10">
               <div className="register__form-register__input">
                 <input
                   type="text"
                   name="firstname"
-                  id=""
                   placeholder="First name"
+                  value={firstname}
+                  onChange={(e) => setRegister({ ...register, firstname: e.target.value })}
                 />
               </div>
               <div className="register__form-register__input">
                 <input
                   type="text"
-                  name="lastname"
-                  id=""
                   placeholder="Last name"
+                  value={lastname}
+                  onChange={(e) => setRegister({ ...register, lastname: e.target.value })}
                 />
               </div>
               <div className="register__form-register__input">
-                <input type="email" name="email" id="" placeholder="Email" />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setRegister({ ...register, email: e.target.value })} />
               </div>
               <div className="register__form-register__input">
                 <input
                   type="text"
-                  name="username"
-                  id=""
                   placeholder="Username"
+                  value={username}
+                  onChange={(e) => setRegister({ ...register, username: e.target.value })}
                 />
               </div>
               <div className="register__form-register__input">
                 <input
                   type="password"
-                  name="password"
-                  id=""
                   placeholder="Password"
+                  value={password}
+                  onChange={(e) => setRegister({ ...register, password: e.target.value })}
                 />
               </div>
               <div className="register__form-register__btn">
-                <button type="submit" className="">
+                <button
+                  onClick={handleSubmit}
+                >
                   Sign up
                 </button>
               </div>
-            </form>
+            </div>
             <div className=" mx-auto mt-3 mb-4 col-10">Do you have account? Login <Link to
-            ="/login">Here</Link></div>
+              ="/login">Here</Link></div>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
