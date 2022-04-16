08b13d0f0c5792bbf66bff48bec69a113cb48554
maint2802 <mai28022000@gmail.com>
2022-04-16 20:32:51 +0700

    add message add to cart

---
+++ frontend/src/components/message/AddToCartSuccess.js
@@ -0,0 +1,15 @@
+
+import './AddToCartSuccess.scss'
+function AddToCartSuccess() {
+
+    return (
+        <div div className="cart-success-form" >
+            <span><i class="fas fa-check-circle"></i></span>
+            <div className="cart-text">Sản phẩm đã được thêm vào giỏ hàng</div>
+        </div>
+
+    )
+}
+
+
+export default AddToCartSuccess
